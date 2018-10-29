2d2db732252d76ab3489a19f4602dd89f3de628b
Pavani Vithala <pavani.vithala18@gmail.com>
2018-10-29 00:03:51 -0400

    Draft2

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,64 +1,58 @@
-var animals = ["Dog", "Cat", "fish", "snake"];
+var animals = [];
 
 // displayMovieInfo function re-renders the HTML to display the appropriate content
 $(document).ready(function() {
-
+  renderButtons();
 function displayAnimalInfo() {
 
   var animal = $(this).attr("data-name");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=qDIj6PV4VSIhhGJn2UABWMDlHmgw0kf0&q=" + animal + "&limit=25&offset=0&rating=G&lang=en";
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=qDIj6PV4VSIhhGJn2UABWMDlHmgw0kf0&q=" + animal + "&limit=10&rating=G";
   
-
+  console.log(queryURL);
   // Creating an AJAX call for the specific movie button being clicked
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-
+    console.log(response);
+    //var result = jQuery.parseJSON(response.d);
+    //console.log("The result is "+result);
     // Creating a div to hold the movie
+    var results = response.data;
     var animalDiv = $("<div class='animal'>");
 
     // Storing the rating data
-    var rating = response.Rated;
+    
 
     // Creating an element to have the rating displayed
-    var pOne = $("<p>").text("Rating: " + rating);
-
+    
     // Displaying the rating
     animalDiv.append(pOne);
 
-    // Storing the release year
-    var released = response.Released;
-
-    // Creating an element to hold the release year
-    var pTwo = $("<p>").text("Released: " + released);
-
-    // Displaying the release year
-    animalDiv.append(pTwo);
-
-    // Storing the plot
-    var plot = response.Plot;
-
-    // Creating an element to hold the plot
-    var pThree = $("<p>").text("Plot: " + plot);
-
-    // Appending the plot
-    animalDiv.append(pThree);
-
     // Retrieving the URL for the image
-    var imgURL = response.Poster;
+    for(var i=0;i<10;i++)
+    {
+      var rating = results[i].rating;
+     console.log("Rating is" + rating);
+     var pOne = $("<p>").text("Rating: " + rating);
 
-    // Creating an element to hold the image
+      imgURL = results[i].images.fixed_height.url;
+      console.log(imgURL);
+     
     var image = $("<img>").attr("src", imgURL);
 
     // Appending the image
-    animalDiv.append(image);
+    animalDiv.append(pOne);
+   animalDiv.append(image);
 
     // Putting the entire movie above the previous movies
     $("#animal-view").prepend(animalDiv);
-  });
+   
+    } 
+  }); 
+}   
+ 
 
-}
 
 
 
@@ -94,6 +88,7 @@ $("#add-animal").on("click", function(event) {
 
   // Adding movie from the textbox to our array
   animals.push(animal);
+  $("#animal-input").val("");
   
   console.log(animal);
 
@@ -104,8 +99,8 @@ $("#add-animal").on("click", function(event) {
 });
 
 // Adding a click event listener to all elements with a class of "movie-btn"
-$(document).on("click", ".movie-btn", displayAnimalInfo);
+$(document).on("click", ".animal-btn", displayAnimalInfo);
 
 // Calling the renderButtons function to display the intial buttons
-renderButtons();
+
 });
