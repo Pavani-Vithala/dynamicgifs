a30e22af1e081a1327c44d4a5c653403335d7013
Pavani Vithala <pavani.vithala18@gmail.com>
2018-10-31 21:18:01 -0400

    Draft 8

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,134 +1,102 @@
 var animals = [];
 
-// displayMovieInfo function re-renders the HTML to display the appropriate content
-$(document).ready(function() {
+
+$(document).ready(function () {
+  $("#animal-input").focus();
   renderButtons();
-function displayAnimalInfo() {
+  function displayAnimalInfo() {
 
-  var animal = $(this).attr("data-name");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=qDIj6PV4VSIhhGJn2UABWMDlHmgw0kf0&q=" + animal + "&limit=25";
-  
-  console.log(queryURL);
-  // Creating an AJAX call for the specific movie button being clicked
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-    
-    var results = response.data;
-    var animalDiv = $("<div class='animal'>");
+    var animal = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=qDIj6PV4VSIhhGJn2UABWMDlHmgw0kf0&q=" + animal + "&limit=25";
 
-    
-    
-    
-    animalDiv.append(pOne);
-
-    // Retrieving the URL for the image
-    for(var i=0;i<10;i++)
-    {
-      var rating = results[i].rating;
-      var title = results[i].title;
-     console.log("Rating is" + rating);
-     var pOne = $("<p>").text("Rating: " + rating);
-     var pTwo = $("<p>").text("Title: " + title);
-     //var favBtn = $("<button>").text("Click to add to Fav");
-
-      var imgURL1 = results[i].images.fixed_height_still.url;
-      var imgURL2 = results[i].images.fixed_height.url;
-      
-     
-    var image = $("<img>").attr("src", imgURL1);
-    image.addClass("gif");
-    image.attr("data-animate",imgURL2);
-    image.attr("data-still",imgURL1);
-    image.attr("data-state","still")
-    
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
 
 
-    // Appending the image
-    animalDiv.append(pOne);
-    animalDiv.append(pTwo);
-    //animalDiv.append(pOne);
-    //animalDiv.append(favBtn);
-    animalDiv.append(image);
+      var results = response.data;
+      for (var i = 0; i < 10; i++) {
+        var animalDiv_i = $("<div class='animal'>");
+        var rating = results[i].rating;
+        var title = results[i].title;
+        console.log("Rating is" + rating);
+        var pOne = $("<p>").text("Rating: " + rating);
+        var pTwo = $("<p>").text("Title: " + title);
+        //var favBtn = $("<button>").text("Click to add to Fav");
 
-    $("#animal-view").prepend(animalDiv);
-   
-    } 
-  }); 
-}   
- 
+        var imgURL1 = results[i].images.fixed_height_still.url;
+        var imgURL2 = results[i].images.fixed_height.url;
+        var fav_i = $("#Favs").val();
+        console.log("The favourite is" + fav_i);
+        var image_i = $("<img>").attr("src", imgURL1);
+
+        image_i.addClass("gif");
+        image_i.attr("data-animate", imgURL2);
+        image_i.attr("data-still", imgURL1);
+        image_i.attr("data-state", "still")
+        animalDiv_i.append(pOne);
+        animalDiv_i.append(pTwo);
+        animalDiv_i.append(fav_i);
+        animalDiv_i.append(image_i);
+        animalDiv_i.css('float', 'left');
+        animalDiv_i.css('margin-right', '5px');
+        $("#animal-view").prepend(animalDiv_i);
 
 
-
-
-// Function for displaying movie data
-function renderButtons() {
-
-  // Deleting the animals prior to adding new animals
-  // (this is necessary otherwise you will have repeat buttons)
-  $("#buttons-view").empty();
-
-  // Looping through the array of animals
-  for (var i = 0; i < animals.length; i++) {
-
-    // Then dynamicaly generating buttons for each movie in the array
-    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-    var a = $("<button>");
-    // Adding a class of movie-btn to our button
-    a.addClass("animal-btn");
-    // Adding a data-attribute
-    a.attr("data-name", animals[i]);
-    // Providing the initial button text
-    a.text(animals[i]);
-    // Adding the button to the buttons-view div
-   // $(".animal-btn").attr("background-color","teal");
-    $('.animal-btn').css({"background-color":"teal"});
-   // $(".animal-btn").attr("color","white").
-
-    $("#buttons-view").append(a);
+      }
+    });
   }
-}
 
-// This function handles events where a movie button is clicked
-$("#add-animal").on("click", function(event) {
-  event.preventDefault();
-  // This line grabs the input from the textbox
-  var animal = $("#animal-input").val().trim();
+  function renderButtons() {
 
-  // Adding movie from the textbox to our array
-  animals.push(animal);
-  $("#animal-input").val("");
-  
-  console.log(animal);
+    $("#buttons-view").empty();
+
+    // Looping through the array of animals
+    for (var i = 0; i < animals.length; i++) {
 
 
+      var a = $("<button>");
 
-  // Calling renderButtons which handles the processing of our movie array
-  renderButtons();
-});
+      a.addClass("animal-btn");
 
-// Adding a click event listener to all elements with a class of "movie-btn"
-$(document).on("click", ".animal-btn", displayAnimalInfo);
+      a.attr("data-name", animals[i]);
 
-// Calling the renderButtons function to display the intial buttons
-$(document).on("click",".gif", function() {
-  // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-  var state = $(this).attr("data-state");
-  // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-  // Then, set the image's data-state to animate
-  // Else set src to the data-still value
-  console.log("Entered gif click function");
-  console.log("The state of Gif is "+ state);
-  
-  console.log(this);
-  if (state === "still") {
-    $(this).attr("src", $(this).attr("data-animate"));
-    $(this).attr("data-state", "animate");
-  } else {
-    $(this).attr("src", $(this).attr("data-still"));
-    $(this).attr("data-state", "still");
+      a.text(animals[i]);
+
+      $('.animal-btn').css({ "background-color": "teal" });
+
+
+      $("#buttons-view").append(a);
+    }
   }
-});
+
+
+  $("#add-animal").on("click", function (event) {
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    var animal = $("#animal-input").val().trim();
+    animals.push(animal);
+    $("#animal-input").val("");
+    renderButtons();
+  });
+
+
+  $(document).on("click", ".animal-btn", displayAnimalInfo);
+
+  $(document).on("click", ".gif", function () {
+
+    var state = $(this).attr("data-state");
+
+
+
+
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
 });
